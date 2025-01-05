8a39d2b5c4b979a84920835c659c7eb5f1295bcd
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-01-05 15:45:26 +0530

    Time: 12 ms (28.26%), Space: 57.2 MB (60.13%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.java
+++ 0878-shifting-letters/0878-shifting-letters.java
@@ -1,19 +1,21 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
         int n = shifts.length;
-        int postfixSum[] = new int[n];
-        int total = 0;
+        // int postfixSum[] = new int[n];
+        long totalShifts = 0;
         StringBuilder sb = new StringBuilder();
         for (int i = n - 1; i >= 0; i--) {
-            total += shifts[i];
-            total %=26;
-            postfixSum[i] = total;
-        }
-        for (int i = 0; i < n; i++) {
-            char ch = (char)('a' + (s.charAt(i) - 'a' + postfixSum[i]) % 26);
+            totalShifts += shifts[i];
+            // totalShifts %=26;
+            // postfixSum[i] = total;
+            char ch = (char)('a' + (s.charAt(i) - 'a' + totalShifts) % 26);
             sb.append(ch);
         }
+        // for (int i = 0; i < n; i++) {
+        //     char ch = (char)('a' + (s.charAt(i) - 'a' + postfixSum[i]) % 26);
+        //     sb.append(ch);
+        // }
         // System.out.println(Arrays.toString(postfixSum));
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
