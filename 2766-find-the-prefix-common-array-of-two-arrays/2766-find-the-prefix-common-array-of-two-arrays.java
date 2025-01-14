2a512e8ec2c9e35abb9a8dfabb67a75ee010949e
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-01-14 10:48:01 +0530

    Time: 1 ms (100%), Space: 45.8 MB (16.93%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
         int n = A.length;
-        Set<Integer> setA = new HashSet<>();
-        Set<Integer> setB = new HashSet<>();
         int cmnPrefix[] = new int[n];
+        int freq[] = new int[n + 1];
         int cnt = 0;
-        for(int i=0; i<n; i++){
-            setA.add(A[i]);
-            setB.add(B[i]);
-            if(A[i] == B[i]){
+        for (int i = 0; i < n; i++) {
+            freq[A[i]]++;
+            freq[B[i]]++;
+            if (A[i] == B[i]) {
                 cnt++;
-            }else if(setB.contains(A[i]) && setA.contains(B[i])){
-                cnt +=2;
-            }else if(setB.contains(A[i]) || setA.contains(B[i])){
-                cnt++;
+            } else {
+                if (freq[A[i]] == 2) {
+                    cnt++;
+                }
+                if (freq[B[i]] == 2) {
+                    cnt++;
+                }
             }
-            
-            cmnPrefix[i]=cnt;
+            cmnPrefix[i] = cnt;
         }
         return cmnPrefix;
     }
