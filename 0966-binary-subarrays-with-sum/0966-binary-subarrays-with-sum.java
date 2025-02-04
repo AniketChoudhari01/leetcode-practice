0745221fc0eba88d306d0f5fa27f9d3aa65b58d4
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-02-04 13:10:30 +0530

    Time: 3 ms (55.33%), Space: 47.5 MB (75.79%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,19 +1,38 @@
 class Solution {
+    // private int slidingWindowAtMost(int nums[], int goal) {
+    // int n = nums.length;
+    // int currSum = 0, total = 0;
+    // int right = 0, left = 0;
+    // while (right < n && left < n) {
+    // currSum += nums[right++];
+    // while (left < right && currSum > goal) {
+    // currSum -= nums[left++];
+    // }
+    // total += (right - left + 1);
+    // }
+    // return total;
+    // }
+
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int cnt = 0;
-        int left = 0, right = 0;
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                if (sum > goal) {
-                    break;
-                } else if (sum == goal) {
-                    cnt++;
-                }
+        int right = 0, left = 0;
+        int total = 0, prefixZeros = 0, currSum = 0;
+        while (right < n) {
+            currSum += nums[right];
+            while (left < right && ( nums[left]==0|| currSum > goal)) {
+                if (nums[left] == 0)
+                    prefixZeros++;
+                else
+                    prefixZeros = 0;
+                currSum -= nums[left];
+                left++;
             }
+            if (currSum == goal) {
+                total += (1 + prefixZeros);
+            }
+            right++;
         }
-        return cnt;
+        return total;
+        // return slidingWindowAtMost(nums, goal) - slidingWindowAtMost(nums, goal - 1);
     }
 }
