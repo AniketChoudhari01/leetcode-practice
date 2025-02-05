600c80692bc80a3876cd810965b799f05f848de9
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-02-05 19:00:06 +0530

    Time: 0 ms (100%), Space: 41.7 MB (58.39%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -4,24 +4,40 @@ class Solution {
             return true;
         }
         int n = s1.length();
-        int freq1[] = new int[26];
-        int freq2[] = new int[26];
         int swaps = 0;
-        for(int i=0; i<n; i++){
-            char ch1 = s1.charAt(i);
-            char ch2 = s2.charAt(i);
-            if(ch1 != ch2){
+        int i = 0, j = 0;
+        for(int k = 0; k<n; k++){
+            if(s1.charAt(k) != s2.charAt(k)){
                 swaps++;
+                if(swaps > 2){
+                    return false;
+                }else if(swaps == 1){
+                    i = k;
+                }else{
+                    j = k;
+                }
             }
-            freq1[ch1 - 'a']++;
-            freq2[ch2 - 'a']++;
         }
-        if(!Arrays.equals(freq1, freq2)){
-            return false;
-        }
-        if(swaps <= 2){
-            return true;
-        }
+        if(s1.charAt(i) == s2.charAt(j) && s1.charAt(j) == s2.charAt(i)) return true;
+
+        // int freq1[] = new int[26];
+        // int freq2[] = new int[26];
+        // int swaps = 0;
+        // for(int i=0; i<n; i++){
+        //     char ch1 = s1.charAt(i);
+        //     char ch2 = s2.charAt(i);
+        //     if(ch1 != ch2){
+        //         swaps++;
+        //     }
+        //     freq1[ch1 - 'a']++;
+        //     freq2[ch2 - 'a']++;
+        // }
+        // if(!Arrays.equals(freq1, freq2)){
+        //     return false;
+        // }
+        // if(swaps <= 2){
+        //     return true;
+        // }
         return false;
     }
 }
