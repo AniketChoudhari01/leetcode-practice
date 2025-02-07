c734f31703764b55cee460b5caea3ca3b9ff8d18
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-02-07 21:20:34 +0530

    Time: 2 ms (99.44%), Space: 55 MB (78.37%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
@@ -2,25 +2,17 @@ class Solution {
     public int maxScore(int[] cardPoints, int k) {
 
         int n = cardPoints.length;
-        int maxi = 0;
-        int prefixSum[] = new int[n];
-        int temp = 0;
-        for (int i = 0; i < n; i++) {
-            temp += cardPoints[i];
-            prefixSum[i] = temp;
+        int leftWindow = 0, rightWindow = 0;
+        for (int i = 0; i < k; i++) {
+            leftWindow += cardPoints[i];
         }
-        if (n == k)
-            return temp;
-        for (int left = n - k, it = 0; it <= k; left = (left + 1) % n, it++) {
-            int total = temp - prefixSum[(left - 1 + n) % n];
-            int rem = k;
-            if (left != 0) {
-                rem = k - (n - left);
-            }
-            if (rem > 0) {
-                total += prefixSum[(rem - 1 + n) % n];
-            }
-            maxi = Math.max(maxi, total);
+        int maxi = leftWindow;
+        int left = k-1, right = n - 1;
+        while (left >= 0 && right >= n - k) {
+            leftWindow -= cardPoints[left--];
+            rightWindow += cardPoints[right--];
+            // System.out.println(leftWindow+" , "+rightWindow);
+            maxi = Math.max(maxi, (leftWindow + rightWindow));
         }
         return maxi;
     }
