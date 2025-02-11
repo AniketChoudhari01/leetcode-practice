ba7122824d21dff2382b6863ecd5fef1f619a1eb
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-02-11 20:29:41 +0530

    Time: 0 ms (100%), Space: 42 MB (8.05%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,15 +14,27 @@
  * }
  */
 class Solution {
-    private void preOrderTraversal(TreeNode root, List<Integer> preOrder){
-        if(root == null) return;
-        preOrder.add(root.val);
-        preOrderTraversal(root.left, preOrder);
-        preOrderTraversal(root.right, preOrder);
-    }
+    // private void preOrderTraversal(TreeNode root, List<Integer> preOrder){
+    //     if(root == null) return;
+    //     preOrder.add(root.val);
+    //     preOrderTraversal(root.left, preOrder);
+    //     preOrderTraversal(root.right, preOrder);
+    // }
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> preOrder = new ArrayList<>();
-        preOrderTraversal(root, preOrder);
+        if(root == null) return preOrder;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode node = st.pop();
+            preOrder.add(node.val);
+            if(node.right != null){ // due to FILO princilple of stack
+                st.push(node.right);
+            }
+            if(node.left != null){
+                st.push(node.left);
+            }
+        }
         return preOrder;
     }
 }
