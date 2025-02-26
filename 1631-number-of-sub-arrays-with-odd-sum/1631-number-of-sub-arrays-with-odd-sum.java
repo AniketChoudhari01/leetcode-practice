596eb4d5544377d8a003c671167c63bfa336a272
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-02-26 12:16:15 +0530

    Time: 7 ms (82.59%), Space: 56.6 MB (92.36%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
@@ -3,27 +3,19 @@ class Solution {
         int ans  = 0;
         int n = arr.length;
         int MOD = (int)1e9+7;
-        int[] dpEven = new int[n], dpOdd = new int[n];
+        int prefixSum = 0;
+        int evenCnt = 1, oddCnt = 0;//since intialized sum to 0
         for(int i=0; i<n; i++){
-            arr[i] %= 2;
-        }
-        if(arr[n-1]==1){
-            dpOdd[n-1] = 1;
-        }else{
-            dpEven[n-1] = 1;
-        }
-        for(int i= n-2; i>=0; i--){
-            if(arr[i] == 1){
-                dpOdd[i] = (1 + dpEven[i+1])%MOD;
-                dpEven[i] = dpOdd[i+1];
+            prefixSum += arr[i];
+            if(prefixSum % 2 == 0){
+                evenCnt ++;
+                ans = (ans + oddCnt)%MOD;
             }else{
-                dpEven[i] = (1+ dpEven[i+1])%MOD;
-                dpOdd[i] = dpOdd[i+1];
+                oddCnt ++;
+                ans = (ans + evenCnt)%MOD;
             }
         }
-        for(int num: dpOdd){
-            ans = (ans + num)%MOD;
-        }
+
         return ans;
     }
 }
