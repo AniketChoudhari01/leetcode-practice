a4a569d9ed4e946ed8e228aa9f7aaf788dd499f9
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-07 14:40:41 +0530

    Time: 69 ms (84.4%), Space: 42 MB (83.2%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -13,22 +13,16 @@ class Solution {
     }
     public int[] closestPrimes(int left, int right) {
         int ans[] = {-1, -1};
-        int cnt = 0;
         int prev = -1;
-        int next = -1;
-        List<Integer> list = new ArrayList<>();
+        int minDiff = Integer.MAX_VALUE;
         for(int i = left ; i<= right; i++){
             if(!seive[i]){
-                list.add(i);
-            }
-        }
-        if(list.size()<2) return ans;
-        int min = Integer.MAX_VALUE;
-        for(int i=1; i<list.size(); i++){
-            if(min > (list.get(i)- list.get(i-1))){
-                min = list.get(i) - list.get(i-1);
-                ans[0]= list.get(i-1);
-                ans[1] = list.get(i);
+                if(prev != -1 && (i-prev)<minDiff){
+                    minDiff = i- prev;
+                    ans[0] = prev;
+                    ans[1] = i;
+                }
+                prev = i;
             }
         }
         return ans;
