c764aeddc6d9abf6aed19bcc7586a21b5c86f3ea
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-11 14:03:32 +0530

    Time: 1 ms (82.98%), Space: 41.6 MB (93.16%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -3,15 +3,9 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int n1 = word1.length(), n2 = word2.length();
         int i = 0, j = 0;
-        boolean flag = true;
         while (i < n1 && j < n2) {
-            if (flag) {
-                sb.append(word1.charAt(i++));
-                flag = false;
-            } else {
-                sb.append(word2.charAt(j++));
-                flag = true;
-            }
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
         }
         while (i < n1) {
             sb.append(word1.charAt(i++));
