9af7282a158f4fe75665db584b1363d2b0a0a8c2
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-13 12:35:41 +0530

    Time: 3 ms (100%), Space: 129 MB (18.13%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -1,29 +1,23 @@
 class Solution {
-    private boolean canFormZeroArray(int nums[], int[][] queries, int k){
-        int n = nums.length;
-        int d[] = new int[n+1];
-        for(int i=0; i<k; i++){
-            int l = queries[i][0];
-            int r = queries[i][1];
-            int val = queries[i][2];
-            d[l] += val;
-            d[r+1] -= val;
-        }
-        int sum = 0;
-        for(int i=0; i<n; i++){
-            sum += d[i];
-            if(sum < nums[i]) return false;
-        }
-        return true;
-    }
     public int minZeroArray(int[] nums, int[][] queries) {
-        int left = 0; int right = queries.length;
-        if(!canFormZeroArray(nums, queries, right)) return -1;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(canFormZeroArray(nums, queries, mid)) right = mid - 1;
-            else left = mid + 1;
+        //using the concept of line sweep algorithm
+        int n = nums.length;
+        int d[] = new int[n+1];//difference array
+        int k = 0, sum = 0;//num of required queries
+        for(int i=0; i<n; i++){
+            while(sum + d[i] < nums[i]){
+                if(k == queries.length) return -1;//reached end 
+                int l = queries[k][0];
+                int r = queries[k][1];
+                int val = queries[k][2];
+                k++;
+                if(r < i) continue;
+
+                d[Math.max(l, i)] += val;
+                d[r + 1] -= val;
+            }
+            sum += d[i];
         }
-        return left;
+        return k;
     }
 }
