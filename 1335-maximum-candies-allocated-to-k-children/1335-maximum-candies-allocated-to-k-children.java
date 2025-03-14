e51227cae141cc0cc6766199afe7ed2add51e044
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-14 15:10:23 +0530

    Time: 27 ms (60.75%), Space: 63.2 MB (58.6%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
@@ -1,27 +1,24 @@
 class Solution {
-    long maxi ;
-    private boolean isSufficient(long k, long mid, int candies[]){
-        long cnt = 0;
-        for(int candy: candies){
-            cnt += candy / mid;
-        }
-        return cnt >= k;
-    }
-    private int binarySearchOnAnsSpace(long low, long high, long k, int candies[]){
-        if(low > high) return (int)high;
-        long mid = low + (high-low)/2;
-        if(isSufficient(k, mid, candies)){
-            return binarySearchOnAnsSpace(mid + 1, high, k, candies);
-        }else{
-            return binarySearchOnAnsSpace(low, mid - 1, k, candies);
-        }
-    }
+
     public int maximumCandies(int[] candies, long k) {
-        int n = candies.length;//binary search on ans space
-        long maxi = Integer.MIN_VALUE;
-        for(int candy: candies){
-            maxi = Math.max(maxi, candy);
+        int n = candies.length;// binary search on ans space
+        long high = 1;
+        for (int candy : candies) {
+            high = Math.max(high, candy);
         }
-        return binarySearchOnAnsSpace(1, maxi, k, candies);
+        long low = 1;
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            long cnt = 0;
+            for (int candy : candies) {
+                cnt += candy / mid;
+            }
+            if (cnt >= k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return (int) high;
     }
 }
