b9df3a7ad5ed1ee4a29699c76b34c83413c4f5fb
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-14 16:11:51 +0530

    Time: 108 ms (5.15%), Space: 60.3 MB (35.44%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
@@ -0,0 +1,26 @@
+class Solution {
+    private boolean isMinPenalty(int penalty, int nums[], int maxOprs){
+        int cnt = 0;
+        for(int num: nums){
+            cnt += Math.ceil(num/(double)penalty)-1; //take care of int/int division and also -1 since with one operation we can divide the bag into 2 bags
+            if(cnt > maxOprs) return false;
+        }
+        return true;
+    }
+    public int minimumSize(int[] nums, int maxOprs) {
+        int low = 1;
+        int high = 1;
+        for(int num: nums){
+            high = Math.max(high, num);
+        }
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(isMinPenalty(mid, nums, maxOprs)){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
