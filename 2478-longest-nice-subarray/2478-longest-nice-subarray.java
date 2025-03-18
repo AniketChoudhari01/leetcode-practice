b2d5b6505bf77e87d0f903874b4ff8c1f57fc784
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-18 12:53:52 +0530

    Time: 3 ms (98.64%), Space: 56.1 MB (12.81%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,41 +1,25 @@
 class Solution {
-    private boolean isNiceSubarray(int nums[], int mid) {
-        if (mid <= 1)
-            return true;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            boolean isNice = true;
-            int mask = 0;
-            if (i + mid > n) {
-                return false;
-            }
-            for (int j = i; j < n && j < i + mid; j++) {
-                if ((mask & nums[j])!= 0) { //check if there is any overlapping bit in this
-                    isNice = false;
-                    break;
-                }
-                mask |= nums[j];//to carry the unique bits from this number to next num 
-            }
-            if (isNice) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public int longestNiceSubarray(int[] nums) {
         int ans = 1;
         int n = nums.length;
-        // [1, 3, 8, 48, 10]
-        int left = 0, right = n;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (isNiceSubarray(nums, mid)) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int left = 0, right = 0;
+        int usedBits = 0;
+        while (right < n) {
+            // while(right < n && (usedBits & nums[right]) != 0){//expand till satisfys
+            //     //means no overlapping bits 
+            //     usedBits ^= nums[right];
+            //     right++;
+            // }
+            //xor with all the elements 
+            //unitll it doesnot satisfy the condition of and == 0 shrink the window from left half
+            //then compare the ans with max len
+            while(left < right && (usedBits&nums[right]) != 0){//shrink due to  bits overlaps
+                usedBits ^= nums[left];
+                left++;
             }
+            usedBits |= nums[right];
+            ans = Math.max(ans, right - left+1);
+            right++;
         }
         return ans;
     }
