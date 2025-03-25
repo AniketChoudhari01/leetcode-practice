9573774d16857ab54d56049dfec10ba9ac293196
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-25 19:28:13 +0530

    Time: 101 ms (56.14%), Space: 122.8 MB (37.28%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.java
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.java
@@ -1,4 +1,16 @@
 class Solution {
+    private boolean canSlice(int range[][]){
+        Arrays.sort(range, (a,b)-> Integer.compare(a[0], b[0]));
+        int prev = range[0][1];
+        int merged_range = 1;
+        for(int i=1; i<range.length; i++){
+            if(prev <= range[i][0]){
+                merged_range++;
+            }
+            prev = Math.max(prev, range[i][1]);
+        }
+        return merged_range >= 3;
+    }
     public boolean checkValidCuts(int n, int[][] rectangles) {
         int N = rectangles.length;
         int start[][] = new int[N][2];
@@ -9,60 +21,60 @@ class Solution {
             end[i][0] = rectangles[i][1];
             end[i][1] = rectangles[i][3];
         }
-        Arrays.sort(start,
-                (a, b) -> {
-                    if (a[0] == b[0]) {
-                        return Integer.compare(a[1], b[1]);
-                    } else {
-                        return Integer.compare(a[0], b[0]);
-                    }
-                });
-        // for (int a[] : start) {
-        //     System.out.println(Arrays.toString(a));
-        // }
-        Arrays.sort(end,
-                (a, b) -> {
-                    if (a[0] == b[0]) {
-                        return Integer.compare(a[1], b[1]);
-                    } else {
-                        return Integer.compare(a[0], b[0]);
-                    }
-                });
+        // Arrays.sort(start,
+        //         (a, b) -> {
+        //             if (a[0] == b[0]) {
+        //                 return Integer.compare(a[1], b[1]);
+        //             } else {
+        //                 return Integer.compare(a[0], b[0]);
+        //             }
+        //         });
+        // // for (int a[] : start) {
+        // //     System.out.println(Arrays.toString(a));
+        // // }
+        // Arrays.sort(end,
+        //         (a, b) -> {
+        //             if (a[0] == b[0]) {
+        //                 return Integer.compare(a[1], b[1]);
+        //             } else {
+        //                 return Integer.compare(a[0], b[0]);
+        //             }
+        //         });
 
-        int lx = start[0][0];
-        int rx = start[0][1];
-        int ly = end[0][0];
-        int ry = end[0][1];
-        int cnt_merged_x = 1;
-        int cnt_merged_y = 1;
-        for (int i = 1; i < rectangles.length; i++) {
-            int l1 = start[i][0];
-            int r1 = start[i][1];
-            int l2 = end[i][0];
-            int r2 = end[i][1];
-            // logic for x path
-            if (l1 < rx) {
-                if (r1 > rx) {
-                    rx = r1;
-                    // System.out.println(" l1, r1 "+l1+" , "+r1);
-                }
-            } else {
-                cnt_merged_x++;
-                lx = l1;
-                rx = r1;
-            }
-            // System.out.println(lx + " " + rx);
-            if (l2 < ry) {
-                if (r2 > ry) {
-                    ry = r2;
-                }
-            } else {
-                cnt_merged_y++;
-                ly = l2;
-                ry = r2;
-            }
-        }
+        // int lx = start[0][0];
+        // int rx = start[0][1];
+        // int ly = end[0][0];
+        // // int ry = end[0][1];
+        // int cnt_merged_x = 1;
+        // int cnt_merged_y = 1;
+        // for (int i = 1; i < rectangles.length; i++) {
+        //     int l1 = start[i][0];
+        //     int r1 = start[i][1];
+        //     int l2 = end[i][0];
+        //     int r2 = end[i][1];
+        //     // logic for x path
+        //     if (l1 < rx) {
+        //         if (r1 > rx) {
+        //             rx = r1;
+        //             // System.out.println(" l1, r1 "+l1+" , "+r1);
+        //         }
+        //     } else {
+        //         cnt_merged_x++;
+        //         lx = l1;
+        //         rx = r1;
+        //     }
+        //     // System.out.println(lx + " " + rx);
+        //     if (l2 < ry) {
+        //         if (r2 > ry) {
+        //             ry = r2;
+        //         }
+        //     } else {
+        //         cnt_merged_y++;
+        //         ly = l2;
+        //         ry = r2;
+        //     }
+        // }
         // System.out.println(cnt_merged_x + " , " + cnt_merged_y);
-        return (cnt_merged_x > 2 || cnt_merged_y > 2);
+        return (canSlice(start) || canSlice(end));
     }
 }
