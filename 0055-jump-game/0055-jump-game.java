22428ac4e00aee13818cd73589dc42b4ebd63475
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-27 18:19:59 +0530

    Time: 1 ms (99.91%), Space: 45.6 MB (84.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,27 +1,13 @@
 class Solution {
-    Boolean isPossible[];
-    private boolean jump(int nums[], int start){
-        if(start >= nums.length - 1){
-            return true;
-        }
-        if(isPossible[start] != null){
-            return isPossible[start]; // return the cached result
-        }
-
-        if(nums[start] == 0){
-            return isPossible[start] = false;//not possible
-        }
-        int maxJump = Math.min(start + nums[start], nums.length-1);
-        for(int i=start + 1; i<=maxJump; i++){
-            if (jump(nums, i)) {
-                return isPossible[start] = true;
+    public boolean canJump(int[] nums) {
+        int lastGoodIndex = nums.length - 1;
+        
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= lastGoodIndex) {
+                lastGoodIndex = i;
             }
         }
-        return isPossible[start] = false;
-    }
-    public boolean canJump(int[] nums) {
-        if(nums.length == 1) return true;
-        isPossible = new Boolean[nums.length];
-        return jump(nums, 0);
+        
+        return lastGoodIndex == 0;
     }
 }
