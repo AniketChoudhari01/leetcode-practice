1a813110334ccfe58d709a9255e6673fa88558cf
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-03-27 11:28:20 +0530

    Time: 1 ms (91.25%), Space: 43.2 MB (11.94%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,26 +1,24 @@
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
         if(nums.length == 1) return 1;
-        int len = 1;
+        int inc_len = 1;
+        int dec_len = 1;
         int n = nums.length;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(j==i) continue;
-                if(nums[j] <= nums[j-1]){
-                    break;
-                }
-                len = Math.max(len, j-i+1);
+        int maxi = 1;
+        for(int i=1; i<n; i++){
+            if(nums[i] > nums[i-1]){
+                inc_len ++;
+                dec_len = 1;
+                maxi = Math.max(maxi, Math.max(inc_len, dec_len));
+            }else if(nums[i] < nums[i-1]){
+                dec_len++;
+                inc_len = 1;
+                maxi = Math.max(maxi, Math.max(inc_len, dec_len));
+            }else{
+                dec_len = 1;
+                inc_len = 1;
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(j==i) continue;
-                if(nums[j] >= nums[j-1]){
-                    break;
-                }
-                len = Math.max(len, j-i+1);
-            }
-        }
-        return len;
+        return maxi;
     }
 }
