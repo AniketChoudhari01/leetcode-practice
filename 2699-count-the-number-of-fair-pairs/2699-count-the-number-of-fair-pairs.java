f0073827a008cea85484159a7d1c2a2eac70280a
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-04-19 13:11:59 +0530

    Time: 76 ms (5.68%), Space: 57.3 MB (71.77%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -1,58 +1,40 @@
 class Solution {
-    private int lowerBound(int nums[], int targetValue, int low, int high) {
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] >= targetValue) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+    private int upperBound(int nums[], int low, int high, int target) {
+        if (low > high) {
+            return low;
         }
-        return low;
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > target) {
+            return upperBound(nums, low, mid - 1, target);
+        } else {
+            return upperBound(nums, mid + 1, high, target);
+        }
     }
 
-    // private int upperBound(int nums[], int targetValue, int start) {
-    //     int low = start;
-    //     int high = nums.length - 1;
-    //     int idx = 0;
-    //     while (low <= high) {
-    //         int mid = (low + high) / 2;
-    //         if (nums[mid] > targetValue) {
-    //             high = mid - 1;
-    //         } else {
-    //             low = mid + 1;
-    //         }
-    //     }
-    //     return low;
-    // }
+    private int lowerBound(int nums[], int low, int high, int target) {
+        if (low > high) {
+            return low;
+        }
+        int mid = low + (high - low) / 2;
+        if (nums[mid] < target) {
+            return lowerBound(nums, mid + 1, high, target);
+        } else {
+            return lowerBound(nums, low, mid - 1, target);
+        }
+    }
 
     public long countFairPairs(int[] nums, int lower, int upper) {
-
-        int n = nums.length;
-        long cntPairs = 0;
+        long count = 0;
         Arrays.sort(nums);
-        // Map<Integer, Integer> map = new HashMap<>();
-        // for (int i = 0; i < nums.length; i++) {
-        //     map.put(nums[i], i);
-        // }
-        System.out.println(Arrays.toString(nums));
+        int n = nums.length;
         for (int i = 0; i < n; i++) {
-            //here (i+1) we want to search from next number till end (i!=j && i<j)
-            long lowIdx = lowerBound(nums, (lower - nums[i]), i+1, n-1);
-            long highIdx = lowerBound(nums, (upper + 1 - nums[i]), i+1, n-1);
-            // if (highIdx<n && lowIdx>=0 && (map.get(nums[highIdx]) - map.get(nums[lowIdx]))>0) {
-                long diff = highIdx - lowIdx;
-                if (diff > 0) {
-                    cntPairs += diff;
-                }
-            // }
-            // System.out.println("low:" + lowIdx + " high:" + highIdx);
-            /*
-             * else if(lowIdx==highIdx && i!=lowIdx && i!=highIdx){
-             * cntPairs++;
-             * }
-             */
+            int low = lower - nums[i];
+            int high = upper - nums[i];
+            int left = lowerBound(nums, i + 1, n - 1, low);
+            int right = upperBound(nums, i + 1, n - 1, high);
+            // System.out.println(left+" , "+right);
+            count += (right - left);
         }
-        return cntPairs;
+        return count;
     }
 }
