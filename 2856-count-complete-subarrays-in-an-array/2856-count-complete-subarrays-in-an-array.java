6cdf11e50107fc39d6f457f980a916ae71ab29b3
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-04-24 11:03:47 +0530

    Time: 6 ms (95.49%), Space: 45.2 MB (34.2%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -5,25 +5,21 @@ class Solution {
         for (int i = 0; i < n; i++) visited.add(nums[i]);
         int unique = visited.size();
         if(unique == 1) return n*(n+1)/2;
-        return countAtMostKDistinct(nums, unique) - countAtMostKDistinct(nums, unique -1);
-    }
-
-    private int countAtMostKDistinct(int nums[], int k){
         int left = 0, right = 0;
-        int cnt = 0, n = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
-        while(right < n){
+        int completeSubarrays = 0;
+        while (right < n) {
             map.put(nums[right], map.getOrDefault(nums[right], 0)+1);
-            while(left < right && map.size() > k){
-                map.put(nums[left], map.get(nums[left]) - 1);
+            while(left < right && map.size() == unique){
+                completeSubarrays += (n - right);
+                map.put(nums[left], map.get(nums[left]) -1);
                 if(map.get(nums[left]) == 0){
                     map.remove(nums[left]);
                 }
                 left++;
             }
-            cnt += (right - left + 1);
             right++;
         }
-        return cnt;
+        return completeSubarrays;
     }
 }
