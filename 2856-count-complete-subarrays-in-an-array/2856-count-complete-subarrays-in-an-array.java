0839dfdae42629b8cf27835abc0d1f273003323a
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-04-24 10:51:31 +0530

    Time: 9 ms (47.98%), Space: 44.8 MB (90.26%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -2,30 +2,28 @@ class Solution {
     public int countCompleteSubarrays(int[] nums) {
         int n = nums.length;
         HashSet<Integer> visited = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            if (!visited.contains(nums[i])) {
-                visited.add(nums[i]);
-            }
-        }
+        for (int i = 0; i < n; i++) visited.add(nums[i]);
         int unique = visited.size();
-        //if condition satified then all the element next in the array will also form subarray
-        int left = 0;
-        Set<Integer> tempSet;
-        int completeSubarrays = 0;
-        while (left < n) {
-            //expand till satisfy condn
-            int right = left;
-            tempSet = new HashSet<>();
-            while (right < n && tempSet.size() < unique) {
-                tempSet.add(nums[right]);
-                right++;
+        if(unique == 1) return n*(n+1)/2;
+        return countAtMostKDistinct(nums, unique) - countAtMostKDistinct(nums, unique -1);
+    }
+
+    private int countAtMostKDistinct(int nums[], int k){
+        int left = 0, right = 0;
+        int cnt = 0, n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while(right < n){
+            map.put(nums[right], map.getOrDefault(nums[right], 0)+1);
+            while(left < right && map.size() > k){
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if(map.get(nums[left]) == 0){
+                    map.remove(nums[left]);
+                }
+                left++;
             }
-            if (tempSet.size() == unique) {
-                completeSubarrays += (n - right + 1);
-                // System.out.println(left+" "+right);
-            }
-            left++;
+            cnt += (right - left + 1);
+            right++;
         }
-        return completeSubarrays;
+        return cnt;
     }
 }
