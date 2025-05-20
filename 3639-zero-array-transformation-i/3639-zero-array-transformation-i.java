b187a2e0b2755b7e79b8d433984dbde3a03dee1b
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-05-20 23:01:52 +0530

    Time: 3 ms (79.52%), Space: 94.8 MB (68.33%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -2,14 +2,20 @@ class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
         int difference[] = new int[n+1];
-        for(int i=0; i<queries.length; i++){
-            difference[queries[i][0]]++;
-            difference[1 + queries[i][1]]--;
+        for(int[] query: queries){
+            difference[query[0]]++;
+            difference[query[1] + 1]--;
         }
         int sum = 0;
+        for(int i=0; i<n+1; i++){
+            sum += difference[i];
+            difference[i] = sum;
+        }
+        // System.out.println(Arrays.toString(difference));
         for(int i=0; i<n; i++){
-            sum += difference[i];//prefix sum
-            if(sum < nums[i]) return false;
+            if(difference[i] < nums[i]){
+                return false;
+            }
         }
         return true;
     }
