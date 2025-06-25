a0c9eb3d380332d3d29dafc54e4d05e6163a7410
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-06-25 11:05:11 +0530

    Time: 1 ms (99.99%), Space: 48.5 MB (52.26%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,34 +1,19 @@
 class Solution {
+    List<Integer> list;
+    private void dfs(int currNum, int n){
+        if(currNum > n) return;
+        list.add(currNum);
+        for(int i = 0; i<10; i++){
+            int next = currNum*10+i;
+            if(next > n) break;
+            dfs(next, n);
+        }
+    }
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> list = new ArrayList<>();
-        int currNum = 1;
-        int i = 1;
-        String arr[] = new String[n];
-        List<String> li = new ArrayList<>();
-        for(int j=1; j<=n; j++){
-            li.add(Integer.toString(j));
+        list = new ArrayList<>();
+        for(int i=1; i<10; i++){
+            dfs(i, n);
         }
-        Collections.sort(li);
-        for(String item:li){
-            list.add(Integer.parseInt(item));
-        }
-        // System.out.println(li);
-
-        // while(i <= n){
-        //     list.add(currNum);
-        //     i++;
-        //     int digit = (int)Math.floor(Math.log10(currNum))+1;
-        //     int tens =  (int)Math.pow(10, digit);
-        //     int leftNum = tens*currNum;
-        //     int rightNum = (currNum+1)*tens;
-        //     while(i <= n && leftNum <= n && leftNum < rightNum){
-        //         list.add(leftNum);
-        //         leftNum++;
-        //         i++;
-        //     }
-        //     currNum++;
-        // }
-        // System.out.println(list);
         return list;
     }
 }
