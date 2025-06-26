b5b990995de2c162c4c7765058ae3cf1bbcaf072
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-06-26 11:06:35 +0530

    Time: 1 ms (100%), Space: 42.1 MB (27.66%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.java
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.java
@@ -2,23 +2,36 @@ class Solution {
     public int longestSubsequence(String s, int k) {
         //we can take all the 0's since it is subsequence they always contribute nothing to the value and increases the length.
         int len = 0;
-        int base = 1;
+        // int base = 1;
         int val = 0;
-        for (char ch : s.toCharArray()) {
-            if (ch != '0')
-                continue;
-            len++;
-        }
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == '1') {
-                if (base + val <= k) {
-                    val += base;
+        // for (char ch : s.toCharArray()) {
+        //     if (ch != '0')
+        //         continue;
+        //     len++;
+        // }
+        // for (int i = s.length() - 1; i >= 0; i--) {
+        //     if (s.charAt(i) == '1') {
+        //         if (base + val <= k) {
+        //             val += base;
+        //             len++;
+        //         }
+        //     }
+        //     base *= 2;
+        //     if (base > k)
+        //         break;
+        // }
+        int numOfBits = (int)(Math.log(k)/Math.log(2)) + 1;
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            int currBit = s.charAt(n-i-1) - '0';
+            if(currBit == 1){
+                if(i < numOfBits && val + (1 << i) <= k){
+                    val += 1<<i;
                     len++;
                 }
+            }else{
+                len++;
             }
-            base *= 2;
-            if (base > k)
-                break;
         }
         return len;
     }
