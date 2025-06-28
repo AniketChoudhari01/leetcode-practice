3f971c27faa581d14db299166f43bc6de3ab289a
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-06-28 15:21:48 +0530

    Time: 0 ms (100%), Space: 44.5 MB (20.69%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,47 +14,14 @@
  * }
  */
 class Solution {
-    List<Integer> inorder;
-    private void inorderTraversal(TreeNode root){
-        if(root == null) return;
-        inorderTraversal(root.left);
-        inorder.add(root.val);
-        inorderTraversal(root.right);
+    private boolean checkBST(TreeNode root, Long min, Long max){
+        if(root == null) return true;
+        if(root.val <= min || root.val >= max) return false;
+        boolean leftCheck = checkBST(root.left, min, (long)root.val);
+        boolean rightCheck = checkBST(root.right, (long)root.val, max);
+        return leftCheck && rightCheck;
     }
     public boolean isValidBST(TreeNode root) {
-        if (root == null || (root.left == null && root.right == null))
-            return true;
-        // TreeNode currNode = root;
-        //morris inorder traversal
-        inorder = new ArrayList<>();
-        inorderTraversal(root);
-        // while (currNode != null) {
-        //     if (currNode.left == null) {
-        //         inorder.add(currNode.val);
-        //         currNode = currNode.right;
-        //     } else {
-        //         TreeNode temp = currNode.left;
-        //         while (temp.right != null && temp.right != currNode) {
-        //             temp = temp.right;
-        //         }
-        //         if (temp.right == null) {
-        //             temp.right = currNode;
-        //             currNode = currNode.left;
-        //         }
-        //         if (temp.right == currNode) {
-        //             temp.right = null;
-        //             inorder.add(currNode.val);
-        //             currNode = currNode.right;
-        //         }
-        //     }
-        // }
-        int prev = inorder.get(0);
-        for(int i = 1; i<inorder.size(); i++){
-            int curr = inorder.get(i);
-            if(curr <= prev) return false;
-            prev = curr;
-        }
-        // System.out.println(inorder);
-        return true;
+        return checkBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 }
