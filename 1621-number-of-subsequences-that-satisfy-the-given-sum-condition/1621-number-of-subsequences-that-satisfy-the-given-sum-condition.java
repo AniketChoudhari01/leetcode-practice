34dcd3bedd1f28d9a682c408c71f5ae2671dae06
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-06-29 16:02:52 +0530

    Time: 56 ms (5.9%), Space: 55.3 MB (53.39%) - LeetHub

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,35 @@
+class Solution {
+    private int lowerBound(int low, int high, int x, int nums[]){
+        if(low > high) return high;
+        int mid = low + (high-low)/2;
+        if(nums[mid] <= x){
+            return lowerBound(mid+1, high, x, nums);
+        }else{
+            return lowerBound(low, mid-1, x, nums);
+        }
+    }
+    public int numSubseq(int[] nums, int target) {
+        int n = nums.length;
+        int MOD = (int)1e9+7;
+        Arrays.sort(nums);
+        int powerOfTwo[] = new int[n];
+        powerOfTwo[0] = 1;
+        for(int i=1; i<n; i++){
+            powerOfTwo[i] = (powerOfTwo[i-1]*2)%MOD;
+        }
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            int j = lowerBound(i+1, n-1, target - nums[i], nums);
+            // if(j < i) continue;
+            if(i == j){
+                if(nums[i]*2 <= target){
+                    ans = (ans + 1)%MOD;
+                }
+            }else{
+                ans = (ans + powerOfTwo[j-i])%MOD;
+            }
+            // System.out.println(j);
+        }
+        return ans;
+    }
+}
