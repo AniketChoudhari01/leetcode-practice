4a5833b39bafa6c0f071c2074a87f8f4778ad848
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-06-29 16:05:04 +0530

    Time: 27 ms (99.52%), Space: 55.3 MB (42.07%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,13 +1,13 @@
 class Solution {
-    private int lowerBound(int low, int high, int x, int nums[]){
-        if(low > high) return high;
-        int mid = low + (high-low)/2;
-        if(nums[mid] <= x){
-            return lowerBound(mid+1, high, x, nums);
-        }else{
-            return lowerBound(low, mid-1, x, nums);
-        }
-    }
+    // private int lowerBound(int low, int high, int x, int nums[]){
+    //     if(low > high) return high;
+    //     int mid = low + (high-low)/2;
+    //     if(nums[mid] <= x){
+    //         return lowerBound(mid+1, high, x, nums);
+    //     }else{
+    //         return lowerBound(low, mid-1, x, nums);
+    //     }
+    // }
     public int numSubseq(int[] nums, int target) {
         int n = nums.length;
         int MOD = (int)1e9+7;
@@ -18,18 +18,27 @@ class Solution {
             powerOfTwo[i] = (powerOfTwo[i-1]*2)%MOD;
         }
         int ans = 0;
-        for(int i=0; i<n; i++){
-            int j = lowerBound(i+1, n-1, target - nums[i], nums);
-            // if(j < i) continue;
-            if(i == j){
-                if(nums[i]*2 <= target){
-                    ans = (ans + 1)%MOD;
-                }
+        int left = 0, right = n-1;
+        while(left <= right){
+            if(nums[left]+nums[right] <= target){
+                ans = (ans + powerOfTwo[right-left])%MOD;
+                left++;
             }else{
-                ans = (ans + powerOfTwo[j-i])%MOD;
+                right--;
             }
-            // System.out.println(j);
         }
+        // for(int i=0; i<n; i++){
+        //     int j = lowerBound(i+1, n-1, target - nums[i], nums);
+        //     // if(j < i) continue;
+        //     if(i == j){
+        //         if(nums[i]*2 <= target){
+        //             ans = (ans + 1)%MOD;
+        //         }
+        //     }else{
+        //         ans = (ans + powerOfTwo[j-i])%MOD;
+        //     }
+        //     // System.out.println(j);
+        // }
         return ans;
     }
 }
