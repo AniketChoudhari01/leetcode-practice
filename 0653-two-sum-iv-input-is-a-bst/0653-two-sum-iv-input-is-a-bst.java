71ee9b57b70ce3639aa72d5093ea2469d4e30198
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-06-30 10:48:26 +0530

    Time: 3 ms (64.78%), Space: 45 MB (85.75%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,27 +14,33 @@
  * }
  */
 class Solution {
-    List<Integer> inorder;
-    private void getInorder(TreeNode root){
-        if(root == null) return;
-        getInorder(root.left);
-        inorder.add(root.val);
-        getInorder(root.right);
-    }
+    // List<Integer> inorder;
+    // private void getInorder(TreeNode root){
+    //     if(root == null) return;
+    //     getInorder(root.left);
+    //     inorder.add(root.val);
+    //     getInorder(root.right);
+    // }
+    Set<Integer> set = new HashSet<>();
     public boolean findTarget(TreeNode root, int k) {
-        inorder = new ArrayList<>();
-        getInorder(root);
-        int left = 0, right = inorder.size()-1;
-        while(left < right){
-            int currSum = inorder.get(left) + inorder.get(right) ;
-            if(currSum == k){
-                return true;
-            }else if(currSum < k){
-                left++;
-            }else{
-                right--;
-            }
-        }
-        return false;
+        if(root == null) return false;
+        int req = k - root.val;
+        if(set.contains(req)) return true;
+        set.add(root.val);
+        return findTarget(root.left, k) || findTarget(root.right, k);
+        // inorder = new ArrayList<>();
+        // getInorder(root);
+        // int left = 0, right = inorder.size()-1;
+        // while(left < right){
+        //     int currSum = inorder.get(left) + inorder.get(right) ;
+        //     if(currSum == k){
+        //         return true;
+        //     }else if(currSum < k){
+        //         left++;
+        //     }else{
+        //         right--;
+        //     }
+        // }
+        // return false;
     }
 }
