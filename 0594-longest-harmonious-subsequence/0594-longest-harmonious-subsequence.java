7b1845968f5c4c2e27241990144f4b15cd3e569c
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-06-30 10:26:09 +0530

    Time: 16 ms (62.12%), Space: 45.2 MB (94.01%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -1,24 +1,24 @@
 class Solution {
     public int findLHS(int[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        if (map.size() == 1) {
-            return 0;
-        }
-        int startElem = -1;
+        // if (map.size() == 1) {
+        //     return 0;
+        // }
+        // int startElem = -1;
         int maxLHS = 0;
         // System.out.println(map);
-        for (Map.Entry<Integer, Integer> pair : map.entrySet()) {
-            int key = pair.getKey();
+        for (int key: map.keySet()) {
+            // int key = pair.getKey();
             if (map.containsKey(key + 1)) {
-                int currSeqLen = map.get(key);
-                currSeqLen += map.getOrDefault(key + 1, 0);
+                int currSeqLen = map.get(key) + map.get(key+1);
+                // currSeqLen += map.getOrDefault(key + 1, 0);
                 if (maxLHS < currSeqLen) {
                     maxLHS = currSeqLen;
-                    startElem = key;
+                    // startElem = key;
                 }
             }
 
