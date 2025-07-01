918ca2e1ee213d8afcf3ec05e3ed695d384e9c89
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-01 09:25:38 +0530

    Time: 1 ms (87.19%), Space: 42.2 MB (38.84%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int ans = 0;
+        char prev = word.charAt(0);
+        int cnt = 1;
+        for(int i=1; i<word.length(); i++){
+            char curr = word.charAt(i);
+            if(prev == curr){
+                cnt++;
+            }else{
+                ans += cnt - 1;
+                cnt = 1;
+                prev = curr;
+            }
+        }  
+        ans += cnt - 1;     
+        return ans+1;
+    }
+}
