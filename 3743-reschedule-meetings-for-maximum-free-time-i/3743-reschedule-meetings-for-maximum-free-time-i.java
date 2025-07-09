9cd36e965fefbad0e538e9b06a8fd66e74ed658f
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-09 22:50:53 +0530

    Time: 2358 ms (5.56%), Space: 63.3 MB (40%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -1,27 +1,26 @@
 class Solution {
-    public int maxFreeTime(int eventTime, int k, int[] st, int[] et) {
-        int n = st.length;
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        //sliding window
+        //from the start gap befor st and before end time
+        //find the potential candiate to reschedule for a meeting
+        //after that calculate the free time
+        //using since the relative order is to be maintained then it should struck in mind for sliding window approach, for each k group of meetings we can push them at start and the difference will the totoal length - length of each meetings under that k meetings
+        int maxFreeTime = 0;
+        int start = 0;
+        int n = startTime.length;
         int duration[] = new int[n];
-        for(int i=0; i<n; i++){
-            duration[i] = et[i]-st[i];
+        for(int i = 0; i<n; i++){
+            duration[i] = endTime[i] - startTime[i];
         }
-        int maxGap = 0;
-        for(int i=0; i+k <= n; i++){
-            int len = 0;
-            int start = 0;
-            int end = eventTime;
-            for(int j=i; j<i+k; j++){
-                len += duration[j];
+        for(int i = 0; i+k <= n; i++){
+            int newEnd = (i + k < n ? startTime[i+k] : eventTime);
+            int meetDuration = 0;//length of meet
+            for(int j = i; j < i + k && j < n; j++){
+                meetDuration += duration[j];
             }
-            if(i!=0){
-                start = et[i-1];
-            }
-            if(i+k-1 != n-1){
-                end = st[i+k];
-            }
-            int gap = end - start - len;
-            maxGap = Math.max(maxGap, gap);
+            maxFreeTime = Math.max(maxFreeTime, newEnd - start - meetDuration);
+            start = endTime[i];
         }
-        return maxGap;
+        return maxFreeTime;
     }
 }
