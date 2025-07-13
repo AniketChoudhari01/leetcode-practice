49dfcef289e67cb85d56bd738438739ba53e187f
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-13 07:50:26 +0530

    Time: 25 ms (94.1%), Space: 57.8 MB (52.5%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
@@ -1,31 +1,48 @@
 class Solution {
-    private PriorityQueue<Integer> fillQ(int arr[]){
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int item: arr){
-            pq.offer(item);
-        }
-        return pq;
-    }
+    // private PriorityQueue<Integer> fillQ(int arr[]){
+    //     PriorityQueue<Integer> pq = new PriorityQueue<>();
+    //     for(int item: arr){
+    //         pq.offer(item);
+    //     }
+    //     return pq;
+    // }
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
         //step1: thinking of greedy approach
         //step2: mapping the player with the lowest capity of trainee 
         //step3
-        PriorityQueue<Integer> playersQ = fillQ(players);
-        PriorityQueue<Integer> traineeQ = fillQ(trainers);
+        // PriorityQueue<Integer> playersQ = fillQ(players);//O(n*logn)
+        // PriorityQueue<Integer> traineeQ = fillQ(trainers);//O(n*logn)
+        // int matches = 0;
+        // while(!playersQ.isEmpty() && !traineeQ.isEmpty()){//at max will run till O(n)
+        //     int ability = playersQ.poll();
+        //     if(traineeQ.peek() >= ability){
+        //         matches++;
+        //         traineeQ.poll();
+        //         continue;
+        //     }
+        //     while(!traineeQ.isEmpty() && traineeQ.peek() < ability){
+        //         traineeQ.poll();
+        //     }
+        //     if(!traineeQ.isEmpty() && traineeQ.peek() >= ability){
+        //         matches++;
+        //         traineeQ.poll();
+        //     }
+        // }
+
+        //method 2
+        //sort them
+        Arrays.sort(players);
+        Arrays.sort(trainers);
         int matches = 0;
-        while(!playersQ.isEmpty() && !traineeQ.isEmpty()){
-            int ability = playersQ.poll();
-            if(traineeQ.peek() >= ability){
-                matches++;
-                traineeQ.poll();
-                continue;
+        int m = players.length, n = trainers.length;
+        for(int i = 0, j = 0; i<m && j<n ;i++){
+            int ability = players[i];
+            while(j<n && trainers[j] < ability){
+                j++;
             }
-            while(!traineeQ.isEmpty() && traineeQ.peek() < ability){
-                traineeQ.poll();
-            }
-            if(!traineeQ.isEmpty() && traineeQ.peek() >= ability){
+            if(j<n && trainers[j] >=  ability){
+                j++;
                 matches++;
-                traineeQ.poll();
             }
         }
         
