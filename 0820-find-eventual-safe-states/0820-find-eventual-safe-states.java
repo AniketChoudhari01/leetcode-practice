78d80f8a46c46d297088eb13536b4e9bfec74b4b
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-14 19:42:30 +0530

    Time: 9 ms (63.85%), Space: 55.3 MB (86.36%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,42 +1,41 @@
 class Solution {
+    Set<Integer> set;
 
-    private boolean elemsInCycle(int src, boolean recursionStack[], boolean visited[], int[][] graph,
-            boolean notSafe[]) {
-        if (!visited[src] && (graph[src].length > 0) && !notSafe[src]) {
-            visited[src] = true;
-            recursionStack[src] = true;
-            for (int v : graph[src] ) {
-                if (!visited[v] && elemsInCycle(v, recursionStack, visited, graph, notSafe)) {
-                    notSafe[src] = true;
-                    return true;
-                } else if (recursionStack[v]) {
-                    notSafe[src] = true;
-                    return true;
-                }
+    private boolean detectCycle(int graph[][], boolean visited[], boolean reStack[], int node) {
+        if (reStack[node]) 
+            return true;
+        if (visited[node])
+            return false;//no need to explore it
+        visited[node] = true;
+        reStack[node] = true;
+        for (int neighbor : graph[node]) {
+            if (detectCycle(graph, visited, reStack, neighbor)) {
+                set.add(node);//add the current parent to the recursion stack
+                return true;
             }
         }
-
-        recursionStack[src] = false;
+        reStack[node] = false;
         return false;
     }
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int m = graph.length;
-        boolean recursionStack[] = new boolean[m];
-        boolean notSafe[] = new boolean[m];
-        boolean visited[] = new boolean[m];
-        List<Integer> terminalSrc = new ArrayList<Integer>();
-        for (int i = 0; i < m; i++) {
-            if (!visited[i] && elemsInCycle(i, recursionStack, visited, graph, notSafe)) {
-                notSafe[i] = true;
+        int n = graph.length;
+        List<Integer> ans = new ArrayList<>();
+        //find the nodes which are not part of cycle dfs
+        set = new HashSet<>();
+        boolean visited[] = new boolean[n];
+        boolean reStack[] = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                detectCycle(graph, visited, reStack, i);
             }
         }
-        for(int i = 0; i< m; i++){
-            if(!notSafe[i]){
-                terminalSrc.add(i);
+        for(int i=0; i<n ; i++){
+            if(!set.contains(i)){
+                ans.add(i);
             }
         }
-        // System.out.println(Arrays.toString(notSafe));
-        return terminalSrc;
+        // System.out.println(set);
+        return ans;
     }
 }
