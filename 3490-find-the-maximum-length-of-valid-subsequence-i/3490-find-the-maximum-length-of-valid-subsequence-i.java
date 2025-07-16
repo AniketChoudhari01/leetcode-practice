8085f4938b701d90d85ce8716860d6fc49882015
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-16 11:17:01 +0530

    Time: 5 ms (60.66%), Space: 62.2 MB (59.84%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,39 +1,38 @@
 class Solution {
-    private int findAlternateLength(int nums[], int curr, int startIdx){
-        int len = 1;
-        for(int i = startIdx+1; i<nums.length; i++){
-            int elem = nums[i];
-            if(curr != elem){
-                len++;
-                curr = elem;
-            }
-        }
-        return len;
-    }
+    // private int hepler(int nums[], int curr, int startIdx){
+    //     int len = 1;
+    //     for(int i = startIdx+1; i<nums.length; i++){
+    //         int elem = nums[i];
+    //         if(curr != elem){
+    //             len++;
+    //             curr = elem;
+    //         }
+    //     }
+    //     return len;
+    // }
     public int maximumLength(int[] nums) {
         int n = nums.length;
         int cnt1s = 0, cnt0s = 0;
-        int firstZeroIdx = -1;
-        int firstOneIdx = -1;
+        int parity[] = new int[2];
+        // int firstZeroIdx = -1;
+        // int firstOneIdx = -1;
         for(int i=0; i<n; i++){
-            nums[i] %= 2;
-            if(nums[i] == 0){
-                if(firstZeroIdx == -1){
-                    firstZeroIdx = i;
-                }
+            int num = nums[i] % 2;
+            if(num == 0){
                 cnt0s++;
             }else{
-                if(firstOneIdx == -1){
-                    firstOneIdx = i;
-                }
+                // if(firstOneIdx == -1){
+                //     firstOneIdx = i;
+                // }
                 cnt1s++;
             }
+            parity[num] = parity[1 - num] + 1; 
         }
         // System.out.println(Arrays.toString(nums));
-        int pickZeroStart = findAlternateLength(nums, 0, firstZeroIdx);
-        int pickOneStart = findAlternateLength(nums, 1, firstOneIdx);
+        // int pickZeroStart = hepler(nums, 0, firstZeroIdx);
+        // int pickOneStart = hepler(nums, 1, firstOneIdx);
         // System.out.println("zero "+pickZeroStart+ " one "+pickOneStart);
-        int maxi = Math.max(Math.max(cnt1s, cnt0s), Math.max(pickZeroStart, pickOneStart));
+        int maxi = Math.max(Math.max(cnt1s, cnt0s), Math.max(parity[0], parity[1]));
         return maxi;
     }
 }
