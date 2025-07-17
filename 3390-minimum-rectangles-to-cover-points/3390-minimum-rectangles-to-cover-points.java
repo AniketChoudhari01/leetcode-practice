b82d827d4b07f12d8e9d1bf6bfa2aa0f468d3292
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-17 10:13:31 +0530

    Time: 4 ms (100%), Space: 98.1 MB (5.63%) - LeetHub

--- 3390-minimum-rectangles-to-cover-points/3390-minimum-rectangles-to-cover-points.java
+++ 3390-minimum-rectangles-to-cover-points/3390-minimum-rectangles-to-cover-points.java
@@ -1,23 +1,35 @@
 class Solution {
     public int minRectanglesToCoverPoints(int[][] points, int w) {
-        int minRectangles = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Set<Integer> visited = new HashSet<>();
-        for(int point[]: points){
-            int x = point[0];
-            if(!visited.contains(x)){
-                pq.offer(x);
-                visited.add(x);
+        int minRectangles = 1;
+        Arrays.sort(points, (a,b)-> Integer.compare(a[0], b[0]));
+        int curr = points[0][0];
+        // for(int point[]: points){
+        //     System.out.println(Arrays.toString(point));
+        // }
+        for(int i=1; i<points.length; i++){
+            int x = points[i][0];
+            if(x - curr > w){
+                minRectangles++;
+                curr = x;
             }
         }
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // Set<Integer> visited = new HashSet<>();
+        // for(int point[]: points){
+        //     int x = point[0];
+        //     if(!visited.contains(x)){
+        //         pq.offer(x);
+        //         visited.add(x);
+        //     }
+        // }
         
-        while(!pq.isEmpty()){
-            int curr = pq.poll();
-            while(!pq.isEmpty() && pq.peek() <= curr + w){
-                pq.poll();
-            }
-            minRectangles++;
-        }
+        // while(!pq.isEmpty()){
+        //     int curr = pq.poll();
+        //     while(!pq.isEmpty() && pq.peek() <= curr + w){
+        //         pq.poll();
+        //     }
+        //     minRectangles++;
+        // }
         return minRectangles;
     }
 }
