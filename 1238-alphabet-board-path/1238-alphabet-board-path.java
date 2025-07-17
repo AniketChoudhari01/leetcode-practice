c14016aff24e965320fdff81a61c2ad759dc1628
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-17 11:12:48 +0530

    Time: 0 ms (100%), Space: 41.5 MB (67.28%) - LeetHub

---
+++ 1238-alphabet-board-path/1238-alphabet-board-path.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String alphabetBoardPath(String target) {
+        StringBuilder sb = new StringBuilder();
+        int row0 = 0, col0 = 0;
+        for (char ch : target.toCharArray()) {
+            int row = (ch - 'a') / 5;
+            int col = (ch - 'a') % 5;
+            if (row0 == row && col0 == col) {
+                sb.append('!');
+            } else {
+                if (ch == 'z') {
+                    int rowDiff = row - row0;
+                    // System.out.println(rowDiff);
+                    sb.append("D".repeat(rowDiff - 1));
+                    //move left if possible
+                    sb.append("L".repeat(Math.abs(col - col0)));
+                    sb.append("D");//for case z
+                } else {
+                    if (row > row0) {
+                        sb.append("D".repeat(row - row0));
+                    } else {
+                        sb.append("U".repeat(row0 - row));
+                    }
+                    if (col > col0) {
+                        sb.append("R".repeat(col - col0));
+                    } else {
+                        sb.append("L".repeat(col0 - col));
+                    }
+                }
+                sb.append('!');
+            }
+            row0 = row;
+            col0 = col;
+        }
+        return sb.toString();
+    }
+}
