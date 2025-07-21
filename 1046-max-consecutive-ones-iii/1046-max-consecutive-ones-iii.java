68b1f58c651e3fd118c2a975ea090439ae7ee7d5
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-21 10:15:43 +0530

    Time: 4 ms (26.49%), Space: 46.8 MB (32.11%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,24 +1,20 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int n = nums.length;
-        if (k >= n) {
-            return n;
-        }
-        // List<Integer> storeIdx = new ArrayList<>(); expensive due to left shift of elements o(n) time
-        Deque<Integer> queue = new ArrayDeque<>();
-        int maxLen = 0;
-        int cnt = 0, right = 0, left = 0;
-        while (right < n) {
-            if (nums[right] == 0) {
-                cnt++;
-                queue.offer(right);
-                if (cnt > k) {
-                    maxLen = Math.max(maxLen, right - left);
-                    left = queue.poll() + 1;
-                }
+        int left = 0, right = 0;
+        int ones = 0;
+        int concecutiveOne = 0;
+        int windowSize = 0;
+        while(right < n){
+            ones += nums[right];
+            windowSize = right - left + 1;
+            if(windowSize - ones > k){
+                ones -= nums[left];
+                left++;
             }
+            concecutiveOne = Math.max(concecutiveOne, right - left +1);
             right++;
         }
-        return Math.max(maxLen, right - left);
+        return concecutiveOne;
     }
 }
