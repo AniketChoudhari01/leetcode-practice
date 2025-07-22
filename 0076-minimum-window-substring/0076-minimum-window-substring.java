934fd433aed4221ac760e4feef3193b19f9e2b01
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-22 15:06:08 +0530

    Time: 136 ms (9.35%), Space: 44.7 MB (87.6%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,12 +1,12 @@
 class Solution {
-    private static boolean areEqual(int arr1[], int arr2[]){
-        for(int i=0; i<128; i++){
-            if(arr1[i] > arr2[i]){
-                return false;
-            }
-        }
-        return true;
-    }
+    // private static boolean areEqual(int arr1[], int arr2[]){
+    //     for(int i=0; i<128; i++){
+    //         if(arr1[i] > arr2[i]){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     public String minWindow(String s, String t) {
         int m = s.length();
         int n = t.length();
@@ -15,24 +15,47 @@ class Solution {
         for(char ch: t.toCharArray()){
             freq1[(int)ch]++;
         }
+        int cnt = 0; //keeps track of required element count for pattern(t) in s
         int minLen = Integer.MAX_VALUE;
-        StringBuilder sb = new StringBuilder();
-        int freq2[] = new int[128];
+        int startIdx = -1;
         while(right < m){
             char ch = s.charAt(right);
-            freq2[(int)ch]++;
-            while(right - left + 1 >= n && areEqual(freq1, freq2)){
-                if(right - left +1 < minLen){
+            if(freq1[(int)ch] > 0){
+                cnt++;
+            }
+            freq1[(int)ch]--;
+            while(cnt == n){
+                if(right - left + 1 < minLen){
                     minLen = right - left + 1;
-                    sb.setLength(0);
-                    sb.append(s.substring(left, right + 1));
+                    startIdx = left;
                 }
-                
-                freq2[(int)s.charAt(left)]--;
+                char ch2 = s.charAt(left);
+                freq1[(int)ch2]++;
+                if(freq1[(int)ch2] > 0){
+                    cnt--;
+                }
                 left++;
             }
             right++;
         }
-        return sb.toString();
-    }
+        return startIdx != -1 ? s.substring(startIdx, startIdx + minLen) : ""; 
+    // StringBuilder sb = new StringBuilder();
+    // int freq2[] = new int[128];
+    // while(right < m){//O(m)
+    //     char ch = s.charAt(right);
+    //     freq2[(int)ch]++;
+    //     while(right - left + 1 >= n && areEqual(freq1, freq2)){ //O(m)*O(128)
+    //         if(right - left +1 < minLen){
+    //             minLen = right - left + 1;
+    //             sb.setLength(0);
+    //             sb.append(s.substring(left, right + 1));
+    //         }
+    //         freq2[(int)s.charAt(left)]--;
+    //         left++;
+    //     }
+    //     right++;
+    // }
+    // return sb.toString();
+    // //O(N^2)
+}
 }
