b4b606e3d8ecd5bfb1f3c72c3fc4e67dd9e4b4c0
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-22 13:24:52 +0530

    Time: 188 ms (7.11%), Space: 45.8 MB (11.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,53 +1,38 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        // if(s.length() < t.length()){
-        //     return "";
-        // }
-        Map<Character, Integer> sMap = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
-
-        HashSet<Character> set = new HashSet<>();
-        int len = 0;
-        int mini = Integer.MAX_VALUE;
-        for (char ch : t.toCharArray()) {
-            tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
-            if (!set.contains(ch)) {
-                set.add(ch);
-                len++;
+    private static boolean areEqual(int arr1[], int arr2[]){
+        for(int i=0; i<128; i++){
+            if(arr1[i] > arr2[i]){
+                return false;
             }
         }
-        int start = 0, end = 0;
+        return true;
+    }
+    public String minWindow(String s, String t) {
+        int m = s.length();
+        int n = t.length();
         int left = 0, right = 0;
-        int cnt = 0;
-        while (right < s.length()) {
+        int freq1[] = new int[128];
+        for(char ch: t.toCharArray()){
+            freq1[(int)ch]++;
+        }
+        int minLen = Integer.MAX_VALUE;
+        StringBuilder sb = new StringBuilder();
+        int freq2[] = new int[128];
+        while(right < m){
             char ch = s.charAt(right);
-            sMap.put(ch, sMap.getOrDefault(ch, 0) + 1);
-            int freq1 = sMap.get(ch);
-            int freq2 = tMap.getOrDefault(ch, 0);
-            if (tMap.containsKey(ch) && freq1 == freq2) {
-                cnt++;
-            }
-            // System.out.println("left " + left + " right: " + right);
-            // System.out.println("cnt :" + cnt);
-            // System.out.println(sMap);
-            while (cnt == len && left <= right) {
-                ch = s.charAt(left);
-                if (mini > right - left + 1) {
-                    // System.out.println(left + " , " + right);
-                    mini = right - left + 1;
-                    start = left;
-                    end = right;
+            freq2[(int)ch]++;
+            while(right - left + 1 >= n && areEqual(freq1, freq2)){
+                if(right - left +1 < minLen){
+                    minLen = right - left + 1;
+                    sb.setLength(0);
+                    sb.append(s.substring(left, right + 1));
                 }
-                int val = sMap.get(ch);
-                sMap.put(ch, val - 1);
-                if (tMap.containsKey(ch) && sMap.get(ch) < tMap.get(ch)) {
-                    cnt--;
-                }
+                
+                freq2[(int)s.charAt(left)]--;
                 left++;
             }
             right++;
         }
-
-        return mini == Integer.MAX_VALUE ? "" : s.substring(start, end + 1);
+        return sb.toString();
     }
 }
