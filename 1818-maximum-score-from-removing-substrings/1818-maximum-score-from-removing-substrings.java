9572de50af30196d12d197c2321bde4960c9b61a
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-23 09:29:02 +0530

    Time: 16 ms (100%), Space: 45.7 MB (61.64%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -14,29 +14,61 @@ class Solution {
         //aa ba xybb +5
         //aaxybb score = 18
         //to gain x points remove "ab" else "ba"
-        char firstChar = x > y ? 'a' : 'b';
-        char lastChar = x > y ? 'b' : 'a';//lastChar
-        Stack<Character> st = new Stack<>();
+
+        //#method-2
         int score = 0;
+        int aCnt = 0, bCnt = 0;
+        char a = 'a', b = 'b';
+        if(x < y){
+            int temp = x;
+            x = y;
+            y = temp;
+            a = 'b';
+            b = 'a';
+        }
         for(char ch: s.toCharArray()){
-            if(!st.isEmpty() && st.peek() == firstChar && ch == lastChar){
-                score += Math.max(x, y);
-                st.pop();
-                continue;
+            if(ch == a){
+                aCnt++;
+            }else if(ch == b){
+                if(aCnt > 0){
+                    aCnt--;
+                    score += x;
+                }else{
+                    bCnt++;
+                }
+            }else{
+                //calculate answer for if there any remaining in current segment
+                score += Math.min(aCnt, bCnt)*y;
+                aCnt = 0;
+                bCnt = 0;//reset condition
             }
-            st.push(ch);
         }
-        Stack<Character> st2 = new Stack<>();
-        while(!st.isEmpty()){
-            char ch = st.pop();
-            if(!st2.isEmpty() && st2.peek() == firstChar && ch == lastChar){
-                score += Math.min(x, y);
-                st2.pop();
-                continue;
-            }
-            st2.push(ch);
-        }
-        // System.out.println(st);
+        //calculate points for any remaining "ba" pairs at the end
+        score += Math.min(aCnt, bCnt)*y;
         return score;
+        // char firstChar = x > y ? 'a' : 'b';
+        // char lastChar = x > y ? 'b' : 'a';//lastChar
+        // Stack<Character> st = new Stack<>();
+        // int score = 0;
+        // for(char ch: s.toCharArray()){
+        //     if(!st.isEmpty() && st.peek() == firstChar && ch == lastChar){
+        //         score += Math.max(x, y);
+        //         st.pop();
+        //         continue;
+        //     }
+        //     st.push(ch);
+        // }
+        // Stack<Character> st2 = new Stack<>();
+        // while(!st.isEmpty()){
+        //     char ch = st.pop();
+        //     if(!st2.isEmpty() && st2.peek() == firstChar && ch == lastChar){
+        //         score += Math.min(x, y);
+        //         st2.pop();
+        //         continue;
+        //     }
+        //     st2.push(ch);
+        // }
+        // System.out.println(st);
+        // return score;
     }
 }
