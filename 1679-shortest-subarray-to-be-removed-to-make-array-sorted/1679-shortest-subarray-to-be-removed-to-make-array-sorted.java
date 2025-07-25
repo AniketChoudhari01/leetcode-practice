b730742c7b16899f575704bf527aefb7935f0e42
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-25 15:53:05 +0530

    Time: 3 ms (25.41%), Space: 65 MB (62.16%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -1,24 +1,18 @@
 class Solution {
     public int findLengthOfShortestSubarray(int[] arr) {
-        int l=0,r=arr.length-1;
-        while(l+1<arr.length && arr[l]<=arr[l+1]){
-            l++;
-        }
-        if(l==arr.length-1){
-            return 0;
-        }
-        while(r>0 && arr[r]>=arr[r-1]){
-            r--;
-        }
-        int ans=Math.min(arr.length-l-1,r);
-        int i=0;int j=r;
-        while(i<=l && j<arr.length ){
-            if(arr[i]<=arr[j]){
-                ans=Math.min(ans,(j-i-1));
-                i++;
-            }else{
-            j++;
+        //2 pointers approach
+        int right = arr.length-1;
+        while(right >0 && arr[right] >= arr[right-1]){
+            right--;
+        }//find the right most index from there till the end array is increasing
+        int ans = right;
+        int left = 0;
+        while(left < right && (left == 0 || arr[left] >= arr[left-1])){
+            while(right < arr.length && arr[left] > arr[right]){
+                right++;
             }
+            ans = Math.min(ans, right-left-1);
+            left++;
         }
         return ans;
     }
