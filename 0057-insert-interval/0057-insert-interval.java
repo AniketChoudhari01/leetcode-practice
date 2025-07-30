1abd92f2304399dbda6d2c37d8903e84e45f80df
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-07-30 12:48:18 +0530

    Time: 4 ms (9.03%), Space: 45.2 MB (14.82%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,31 +1,27 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list = new ArrayList<>();
-        int n = intervals.length;
-        int arr[][] = new int[n+1][2];
-        boolean isAdded;
-        for(int i=0; i<n; i++){
-            arr[i][0] = intervals[i][0];
-            arr[i][1] = intervals[i][1];
-        }        
-        arr[n][0] = newInterval[0];
-        arr[n][1] = newInterval[1];
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
-        list.add(arr[0]);
-        for(int i=1; i<n+1; i++){
-            int len = list.size();
-            if(list.get(len - 1)[1] >= arr[i][0] ){
-                list.get(len-1)[1] = Math.max(list.get(len-1)[1], arr[i][1]);
+        int m = intervals.length;
+        int temp[][] = new int[m+1][2];
+        int i=0;
+        for(int inter[]:intervals){
+            temp[i][0] = inter[0];
+            temp[i++][1] = inter[1];
+        }
+        temp[m][0] = newInterval[0];
+        temp[m][1] = newInterval[1];
+        Arrays.sort(temp, (a,b)->Integer.compare(a[0], b[0]));
+        int ans[][] = new int[m+1][2];
+        ans[0][0] = temp[0][0];
+        ans[0][1] = temp[0][1];
+        int idx =0;
+        for(int j=1; j<m+1; j++){
+            if(ans[idx][1] >= temp[j][0]){
+                ans[idx][1] = Math.max(ans[idx][1], temp[j][1]);
             }else{
-                list.add(arr[i]);
+                ans[++idx][0] = temp[j][0];
+                ans[idx][1] = temp[j][1];
             }
         }
-        int res[][] = new int[list.size()][2];
-        int i = 0;
-        for(int temp[]: list){
-            res[i][0] = temp[0];
-            res[i++][1] = temp[1];
-        }
-        return res;
+        return Arrays.copyOfRange(ans, 0, idx+1);
     }
 }
