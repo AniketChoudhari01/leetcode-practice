18675b7739941b0869fc7b3386b68c2944a6deb7
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-09-03 09:27:16 +0530

    Time: 3 ms (32.93%), Space: 15.1 MB (18.13%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,58 +1,24 @@
 class Solution {
 public:
-    // int dfs(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp){
-    //     if(i < 0 || j < 0 || j >= matrix[0].size()) return INT_MAX;
-    //     if(i == 0) return matrix[i][j];
-    //     if(dp[i][j] != -1) return dp[i][j];
+    int maxi = 101;
+    int dfs(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp){
+        if(i < 0 || j < 0 || j >= matrix[0].size()) return INT_MAX;
+        if(i == 0) return matrix[i][j];
+        if(dp[i][j] != maxi) return dp[i][j];
 
-    //     int up = dfs(i-1, j, matrix, dp);
-    //     int diag1 = dfs(i-1, j-1, matrix, dp);
-    //     int diag2 = dfs(i-1, j+1, matrix, dp);
+        int up = dfs(i-1, j, matrix, dp);
+        int diag1 = dfs(i-1, j-1, matrix, dp);
+        int diag2 = dfs(i-1, j+1, matrix, dp);
 
-    //     return dp[i][j] = matrix[i][j] + min(min(diag1, diag2), up);
-    // }
+        return dp[i][j] = matrix[i][j] + min(min(diag1, diag2), up);
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
         int mini = INT_MAX;
-        // vector<vector<int>> dp(m, vector<int>(n, 0));
-        // for(int j=0; j<n; j++){
-        //     dp[0][j] = matrix[0][j];
-        // }
-        //top down approach we build our answer from the base case
-            // for(int i = 1; i<m; i++){
-            //     for(int j=0; j<n; j++){
-
-            //         int up = dp[i-1][j];
-            //         int diag1 = (j > 0) ? dp[i-1][j-1] : INT_MAX;
-            //         int diag2 = (j < n-1) ? dp[i-1][j+1] : INT_MAX;
-                    
-            //         dp[i][j] = matrix[i][j] + min({diag1, diag2, up});
-            //     }
-            // }
-        // for(auto &item: dp){
-        //     for(int it: item){
-        //         cout<<it<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
-
-        //space optmization solution
-        vector<int> prev(matrix[0]);
-        for(int i=1; i<m;i++){
-            vector<int> temp(n, 0);
-            for(int j=0; j<n; j++){
-                int up = INT_MAX, diag1 = INT_MAX, diag2 = INT_MAX;
-                up = prev[j];
-                if(j-1>=0) diag1 = prev[j-1];
-                if(j+1<n) diag2 = prev[j+1];
-                
-                temp[j] = matrix[i][j] + min({up, diag1, diag2});
-            }
-            prev = temp;
+        vector<vector<int>> dp(m, vector<int>(n, maxi));
+        for(int j=0; j<n; j++){
+            mini = min(mini, dfs(m-1, j, matrix, dp));
         }
-
-        for(int i=0; i<n; i++) mini = min(mini, prev[i]);
-
         return mini;
     }
 };
