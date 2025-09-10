4b069c3941423343a7a1354bcb03b08d8991df0a
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-09-10 09:27:50 +0530

    Time: 15 ms (75.3%), Space: 10.8 MB (77.68%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -19,27 +19,30 @@ public:
     int change(int amount, vector<int>& coins) {
         //we are having the infinite supply of coins and we have to find the number of combinations that make up that amount when taken we don't need to move index as we don't know when it will give optimal answer.
         int n = coins.size();
-        vector<vector<ll>> dp(n, vector<ll>(amount+1, 0));
+        // vector<vector<ll>> dp(n, vector<ll>(amount+1, 0));
+        //SPACE optimization
+        vector<ll> prev(amount+1, 0), curr(amount+1,0);
         for(int target = 0; target<= amount; target++){
             if(target%coins[0] == 0){
-                dp[0][target] = 1;
+                prev[target] = 1;
             }else{
-                dp[0][target] = 0;
+                prev[target] = 0;
             }
         }
         for(int i=0; i<n; i++){
-            dp[i][0] = 1;
+            prev[0] = 1;
         }
 
         for(int idx=1; idx<n; idx++){
             for(int target = 0; target<= amount; target++){
-                int notTaken = dp[idx-1][target];
+                int notTaken = prev[target];
                 int taken = 0;
-                if(coins[idx] <= target) taken = dp[idx][target-coins[idx]];
+                if(coins[idx] <= target) taken = curr[target-coins[idx]];
 
-                dp[idx][target] = ll(notTaken) + ll(taken);
+                curr[target] = ll(notTaken) + ll(taken);
             }
+            prev = curr;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };
