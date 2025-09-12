0366d80744d60956250707aa36200439703085fe
Aniket Choudhari <cs22b1010aniketchoudhari@gmail.com>
2025-09-12 12:01:43 +0530

    Time: 3 ms (87.13%), Space: 19.2 MB (25.75%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -8,36 +8,38 @@ public:
         //removes the non-empty, odd no. of vowels from string
         //bob even no of substring
         // bool alicwWin = false;
-        int vowels = 0;
+        // int vowels = 0;
         //greedyliy they try to exhaust the vowels
         for(char ch: s){
             if(isVowel(ch)){
-                vowels++;
+                // vowels++;
+                return true;
             }
         }
         //bob can remove 0 vowels as well
-        bool turn = 1;//alice
-        while(vowels > -1){
-            if(turn){
-                if(vowels%2 != 0){
-                    //exhaust all and alice wins
-                    return true;
-                }else if(vowels > 0){
-                    vowels = 1;
-                }else{
-                    return false;//bob wins
-                }
-            }else{
-                if(vowels % 2 == 0) return false;
-                else if(vowels%2 != 0){
-                    //do nothing don't remove any vowels
-                    vowels = 1;//removes all even occurences
-                }else{
-                    return true;
-                }
-            }
-            turn = ~turn;
-        }
-        return (vowels%2 != 0);
+        // bool turn = 1;//alice
+        // while(vowels > -1){
+        //     if(turn){
+        //         if(vowels%2 != 0){
+        //             //exhaust all and alice wins
+        //             return true;
+        //         }else if(vowels > 0){
+        //             vowels = 1;
+        //         }else{
+        //             return false;//bob wins
+        //         }
+        //     }else{
+        //         if(vowels % 2 == 0) return false;
+        //         else if(vowels%2 != 0){
+        //             //do nothing don't remove any vowels
+        //             vowels = 1;//removes all even occurences
+        //         }else{
+        //             return true;
+        //         }
+        //     }
+        //     turn = ~turn;
+        // }
+        // return (vowels%2 != 0);
+        return false;
     }
 };
